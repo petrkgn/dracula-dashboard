2f700e6a63dfb7f81b57a3b037261573c2155d3e
Petr Balashenko <petrsdk@gmail.com>
2022-06-30 21:45:46 +0500

    Muuri grid service changed

--- src/app/shared/services/muuri-grid.service.ts
+++ src/app/shared/services/muuri-grid.service.ts
@@ -1,9 +1,9 @@
-import { Injectable } from '@angular/core';
+import { Injectable } from "@angular/core";
 
-import Muuri, { GridOptions } from 'muuri';
+import Muuri, { GridOptions } from "muuri";
 
 @Injectable({
-  providedIn: 'root',
+  providedIn: "root",
 })
 export class MuuriGridService {
   private muuriGrid!: Muuri;
@@ -18,20 +18,16 @@ export class MuuriGridService {
   }
 
   public addItemToGrid(targetItem: HTMLElement[]) {
-    if (this.muuriGrid) {
-      this.muuriGrid.add(targetItem, { index: 0 });
-    } else {
-      return alert('Muuri grid is not initialized');
-    }
+    this.muuriGrid
+      ? this.muuriGrid.add(targetItem, { index: 0 })
+      : alert("Muuri grid is not initialized");
   }
 
   public removeItemFromGrid(targetItem: HTMLElement) {
-    if (this.muuriGrid) {
-      this.muuriGrid.remove(this.muuriGrid.getItems(targetItem), {
-        removeElements: true,
-      });
-    } else {
-      return alert('Muuri grid is not initialized');
-    }
+    this.muuriGrid
+      ? this.muuriGrid.remove(this.muuriGrid.getItems(targetItem), {
+          removeElements: true,
+        })
+      : alert("Muuri grid is not initialized");
   }
 }
