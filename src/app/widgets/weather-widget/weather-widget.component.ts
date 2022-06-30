fc60dcac874b513983af01e4286862146298d98a
petrkgn <lastmail@yandex.ru>
2022-06-30 11:42:45 +0500

    1 file modified

--- src/app/widgets/weather-widget/weather-widget.component.ts
+++ src/app/widgets/weather-widget/weather-widget.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-weather-widget',
   templateUrl: './weather-widget.component.html',
-  styleUrls: ['./weather-widget.component.css']
+  styleUrls: ['./weather-widget.component.scss']
 })
 export class WeatherWidgetComponent implements OnInit {
 
